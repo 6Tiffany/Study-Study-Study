a06eec03e99466893e3bf327d478bc1f4fc7979c
6Tiffany <104591521+6Tiffany@users.noreply.github.com>
2022-04-28 11:38:24 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,94 @@
+$(".Yes").click(function() {
+    $(".z").hide();
+    $(".p").hide();
+});
+
+
+$(".Yes").click(function() {
+    $(".ll").show();
+    $(".AB").show();
+});
+
+$(".zhi").click(function() {
+    $(".er").show("A");
+});
+
+$(".zhi").click(function() {
+    $(".kl").show();
+});
+
+$(".Yes").click(function() {
+    $(".as").hide();
+});
+
+$(".Yes").click(function() {
+    $(".z").hide();
+});
+
+$(".Yes").click(function() {
+    $(".book").show();
+});
+
+$(".we").click(function() {
+    $(".bn").show();
+});
+
+
+$(".bn").click(function() {
+    $(".Ti").show();
+    $(".happiness").hide();
+});
+
+
+$(".book").click(function() {
+    $(".bn").hide();
+});
+
+$(".we").click(function() {
+    $(".as").hide();
+   
+});
+
+$(".we").click(function() {
+    $(".ok").show();
+        $(".book").show();
+            $(".thanks").hide();
+});
+
+$(".sad").click(function() {
+    $(".Ti").show();
+        $(".teacher").hide();
+
+});
+
+$(".sad").click(function() {
+        $(".green").hide();
+            $(".car").hide();
+                $(".yellow").show();
+});
+
+$(".wan").click(function(){
+    $(".hat").text("Do better next time");
+});
+            
+$(".bn").click(function(){
+    $("body").css("backgound-color","lightblue");
+});
+
+$(".green").dblclick(function(){
+    $(".z").show();
+        $(".p").show();
+    $(".ki").show();
+    $(".we").show();
+    $(".Ti").hide();
+$(".as").show();
+});
+
+$(".happiness").dblclick(function(){
+    $(".z").show();
+        $(".p").show();
+    $(".ki").show();
+    $(".we").show();
+    $(".Ti").hide();
+$(".as").show();
+});
